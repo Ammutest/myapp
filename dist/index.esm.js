2302a7b52ef3ac3a7af8dd675ea4ef3b875ce316
Ammu Lomy <ammulomy2017@gmail.com>
2025-03-17 14:34:19 +0530

    Created

---
+++ dist/index.esm.js
@@ -0,0 +1,1 @@
+import e from"react";const t=()=>e.createElement("h1",null,"Display Component"),n=()=>e.createElement("h1",null,"Name Component");export{t as Display,n as Name};
---
+++ src/components/Name.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Name = () => {
+  return (
+    <h1>Name Component</h1>
+  )
+};
+
+ export default Name;
